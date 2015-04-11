a026adac1d2364cc1c4d90e24b44e05ee0d02434
landyking <landykingdom@163.com>
2015-04-11 18:30:53 +0800

    jdk版本改为1.6

--- src/main/java/com/jfreer/game/ddz/exception/DDZException.java
+++ src/main/java/com/jfreer/game/ddz/exception/DDZException.java
@@ -21,7 +21,4 @@ public class DDZException extends Exception {
         super(cause);
     }
 
-    public DDZException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
